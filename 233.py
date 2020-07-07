4f6be132c9d92c20573cb1744a34c2ca69a271ff
mkasatkin <Flash20031966>
2020-07-07 14:42:45 +0400

    233

--- 233.py
+++ 233.py
@@ -1,25 +1,25 @@
-import os
+import math
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 try:
-    link = "http://suninjuly.github.io/file_input.html"
+    link = "http://suninjuly.github.io/alert_accept.html"
     browser = webdriver.Chrome()
     browser.get(link)
-    x = browser.find_element(By.XPATH, "//input[@name='firstname']")
-    x.send_keys("Зубенко")
-    x = browser.find_element(By.XPATH, "//input[@name='lastname']")
-    x.send_keys("МихаилПетрович")
-    x = browser.find_element(By.XPATH, "//input[@name='email']")
-    x.send_keys("qweasd@qwdsa.qwe")
-    element = browser.find_element(By.XPATH, "// input[ @ id = 'file']")
-    current_dir = os.path.abspath(os.path.dirname(__file__))  # получаем путь к директории текущего исполняемого файла
-    file_path = os.path.join(current_dir, 'file.txt')  # добавляем к этому пути имя файла
-    element.send_keys(file_path)
     button = browser.find_element_by_tag_name("button")
     button.click()
-
+    confirm = browser.switch_to.alert
+    confirm.accept()
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+    xel = browser.find_element(By.XPATH, "//span[@id='input_value']")
+    x = xel.text
+    y = calc(x)
+    q = browser.find_element(By.XPATH, "//input")
+    q.send_keys(str(y))
+    button = browser.find_element_by_tag_name("button")
+    button.click()
 finally:
     time.sleep(10)
     browser.quit()
