d2261a6c6ef256622d553381ceb88e5de6e1a7b6
mkasatkin <Flash20031966>
2020-07-08 09:05:04 +0400

    initial commit

--- 226.py
+++ 226.py
@@ -2,7 +2,6 @@ import time
 import math
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 
 try:
     link = "http://SunInJuly.github.io/execute_script.html"
